6e1772b20433c08497e4e2deb46bafcabb91724f
Miaan <nghiabn2005@gmail.com>
2023-12-30 23:51:02 +0700

    main

---
+++ Utilities/GameEvents/GameEventListener.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    #region Serialized Fields
+    [SerializeField]
+    protected List<GameEvent> events = new();
+
+    [SerializeField]
+    protected UnityEvent responses;
+
+    #endregion
+
+
+    #region MonoBehaviour Callbacks
+
+    protected void OnEnable() 
+    {
+        foreach (GameEvent _event in events)
+        {
+            _event.RegisterListener(this);
+        }
+    }
+
+    protected void OnDisable() 
+    {
+        foreach (GameEvent _event in events)
+        {
+            _event.UnregisterListener(this);
+        }
+    }
+
+    #endregion
+
+
+    #region Public Callbacks
+
+    public virtual void Raise() => responses?.Invoke();
+
+    public virtual void ClearAll() => responses?.RemoveAllListeners();
+
+    #endregion
+}
